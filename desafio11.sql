0e7669c62307f1bdcfaca854f9523dce7e7a865e
carloshtbraga <carloshtbraga@gmail.com>
2023-03-28 16:06:01 -0300

    Requisito 11

---
+++ desafio11.sql
@@ -0,0 +1,6 @@
+SELECT notes FROM purchase_orders
+WHERE notes LIKE 'Purchase generated based on Order #3%';
+
+-- SELECT notes FROM purchase_orders
+-- WHERE notes BETWEEN 'Purchase generated based on Order #30' 
+-- AND 'Purchase generated based on Order #39' 
